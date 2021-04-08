505be755bb4538dc5928f42b6fa2d6730ffeedb9
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-04-08 19:34:39 +0900

    login 페이지로 가게 시큐리티 설정

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,14 +2,54 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
 	// http://localhost:8082/
 	@GetMapping("/")	
-	public String index() {
+	public @ResponseBody String index() {
 		
 		return "index";	// src/main/resources/templates/index.mustache
 	}
+	
+	
+	@GetMapping("/user")	
+	public @ResponseBody String user() {
+		
+		return "user";	
+	}
+	
+	@GetMapping("/manager")	
+	public @ResponseBody String manager() {
+		
+		return "manager";	
+	}
+	
+	@GetMapping("/login")	// 시큐리티 낚아채지 않게 설정!
+	public @ResponseBody String login() {
+		
+		return "login";	
+	}
+	
+	@GetMapping("/join")	
+	public @ResponseBody String join() {
+		
+		return "join";	
+	}
+	
+	@GetMapping("/joinProc")	
+	public @ResponseBody String joinProc() {
+		
+		return "회원가입이 완료됨";	
+	}
+	
+	@GetMapping("/admin")	
+	public @ResponseBody String admin() {
+		
+		return "admin";	
+	}
+	
+	
 }
