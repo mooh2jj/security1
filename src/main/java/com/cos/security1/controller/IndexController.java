46ac0579f22aa1a0ef673592f87273f149be34a4
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-04-08 20:29:57 +0900

    시큐리티 회원가입 페이지 추가 및 패스워드 암호화 설정

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,12 +1,24 @@
 package com.cos.security1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
 
 @Controller
 public class IndexController {
 
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	// http://localhost:8082/
 	@GetMapping("/")	
 	public @ResponseBody String index() {
@@ -27,27 +39,31 @@ public class IndexController {
 		return "manager";	
 	}
 	
-	@GetMapping("/login")	// 시큐리티 낚아채지 않게 설정!
-	public @ResponseBody String login() {
+	@GetMapping("/loginForm")	// 시큐리티 낚아채지 않게 설정!
+	public String loginForm() {
 		
-		return "login";	
+		return "loginForm";	
 	}
 	
-	@GetMapping("/join")	
-	public @ResponseBody String join() {
-		
-		return "join";	
+	@GetMapping("/joinForm")	
+	public String joinForm() {
+//		System.out.println("joinForm controller start...");
+		return "joinForm";	
 	}
 	
-	@GetMapping("/joinProc")	
-	public @ResponseBody String joinProc() {
-		
-		return "회원가입이 완료됨";	
+	@PostMapping("/join")	
+	public String join(User user) {
+		System.out.println("join start... , user: "+user);
+		user.setRole("ROLE_USER");
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		userRepository.save(user);	// 회원가입 잘됨 but, 시큐리티로 로그인을 할 수 없음. 패스워드 암호화가 안되었기 때문에 => bCryptPasswordEncoder 해야됨!
+		return "redirect:/loginForm";	
 	}
 	
 	@GetMapping("/admin")	
 	public @ResponseBody String admin() {
-		
 		return "admin";	
 	}
 	
