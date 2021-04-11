ef5b5ea894cc4d540bebf822b718151fe5f82b9a
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-04-11 22:18:18 +0900

    구글 회원 프로필 정보 받아보기

--- src/main/java/com/cos/security1/config/SecurtiyConfig.java
+++ src/main/java/com/cos/security1/config/SecurtiyConfig.java
@@ -1,5 +1,6 @@
 package com.cos.security1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,10 +9,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.cos.security1.config.oauth.PrincipalOauthUserService;
+
 @Configuration
 @EnableWebSecurity  // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨.
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurtiyConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private PrincipalOauthUserService principalOauthUserService;
 	
 	// 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
 	@Bean
@@ -34,7 +40,9 @@ public class SecurtiyConfig extends WebSecurityConfigurerAdapter{
 			.defaultSuccessUrl("/")
 			.and()
 			.oauth2Login()
-			.loginPage("/loginForm");
+			.loginPage("/loginForm")		// 구글 로그인이 완료된 Tip 코드 x (상태 엑세스토큰 + 사용자 프로필정보)
+			.userInfoEndpoint()
+			.userService(principalOauthUserService);
 	}
 	
 }
