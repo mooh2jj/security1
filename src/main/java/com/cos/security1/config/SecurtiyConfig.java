b86df167066faaaa3ee47fa1681a6a66ac6f8519
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-04-11 21:36:21 +0900

    구글 로그인 준비

--- src/main/java/com/cos/security1/config/SecurtiyConfig.java
+++ src/main/java/com/cos/security1/config/SecurtiyConfig.java
@@ -2,6 +2,7 @@ package com.cos.security1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity  // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨.
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurtiyConfig extends WebSecurityConfigurerAdapter{
 	
 	// 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
@@ -29,7 +31,10 @@ public class SecurtiyConfig extends WebSecurityConfigurerAdapter{
 			.formLogin()
 			.loginPage("/loginForm")
 			.loginProcessingUrl("/login")	// login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
-			.defaultSuccessUrl("/");
+			.defaultSuccessUrl("/")
+			.and()
+			.oauth2Login()
+			.loginPage("/loginForm");
 	}
 	
 }
