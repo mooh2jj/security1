f7e7b27359115ad08c51bab91c7372b3b6611240
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-07-17 12:14:17 +0900

    페이스북 로그인 완료  페이스북 providerId sub -> id

--- src/main/java/com/cos/security1/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/security1/config/auth/PrincipalDetailsService.java
@@ -11,6 +11,7 @@ import com.cos.security1.repository.UserRepository;
 
 // 시큐리티 설정에서 loginProcessingUrl("/logn");
 // /login 요청이 오면 자동으로 UserDetailsService 타입으로 IoC되어 있는 loadUserByUsername 함수가 실행
+// 함수 종료시 @AuthenticationPrincipal 어노테이션이 만들어진다.
 @Service
 public class PrincipalDetailsService implements UserDetailsService{
 
--- src/main/java/com/cos/security1/config/oauth/PrincipalOauthUserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauthUserService.java
@@ -1,6 +1,9 @@
 package com.cos.security1.config.oauth;
 
 import com.cos.security1.config.auth.PrincipalDetails;
+import com.cos.security1.config.oauth.provider.FacebookUserInfo;
+import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,7 @@ public class PrincipalOauthUserService extends DefaultOAuth2UserService {
 	private UserRepository userRepository;
 
 	// 구글로부터 받은 userRequest 데이터에 대한 후처리되는 함수
+	// 함수 종료시 @AuthenticationPrincipal 어노테이션이 만들어진다.
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		System.out.println("getClientRegistration: "+userRequest.getClientRegistration());	// registrationId로 어떤 OAuth로 로그인
@@ -33,17 +37,29 @@ public class PrincipalOauthUserService extends DefaultOAuth2UserService {
 		System.out.println("getAttributes: "+oAuth2User.getAttributes());
 
 		// oauth 회원가입 강제 등록
-		String provider = userRequest.getClientRegistration().getClientId();	// google
-		String providerId = oAuth2User.getAttribute("sub");
+		OAuth2UserInfo oAuth2UserInfo = null;
+
+		if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+			System.out.println("구글 로그인 요청");
+			oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+		} else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")){
+			System.out.println("페이스북 로그인 요청");
+			oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+		}else{
+			System.out.println("우리는 구글과 페이스북만 지원합니다.");
+		}
+
+		String provider = oAuth2UserInfo.getProvider();    // google, facebook
+		String providerId = oAuth2UserInfo.getProviderId();                // facebook일 때는 null
 		String username = provider + "_" + providerId;							// google_1097~~
 		String password = bCryptPasswordEncoder.encode("getitthere");
-		String email = oAuth2User.getAttribute("email");
+		String email = oAuth2UserInfo.getEmail();
 		String role = "ROLE_USER";
 
 		User userEntity = userRepository.findByUsername(username);
 
 		if (userEntity == null) {
-			System.out.println("구글 로그인 최초");
+			System.out.println("로그인 최초");
 			userEntity = User.builder()
 					.username(username)
 					.password(password)
@@ -54,7 +70,7 @@ public class PrincipalOauthUserService extends DefaultOAuth2UserService {
 					.build();
 			userRepository.save(userEntity);
 		} else {
-			System.out.println("구글 로그인을 이미 했음, 자동회원가입이 되어있다.");
+			System.out.println("로그인을 이미 했음, 자동회원가입이 되어있다.");
 		}
 
 		return new PrincipalDetails(userEntity, oAuth2User.getAttributes());
